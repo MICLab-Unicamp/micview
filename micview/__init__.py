af54d5c97a24a8fdb09984e2161637c8398e6583
dscarmo <carmodiedre@outlook.com>
2023-12-05 19:02:25 -0300

    Non blocking option for using openfile

--- micview/__init__.py
+++ micview/__init__.py
@@ -4,12 +4,21 @@ def open():
     import micview.run as run
     run.main([])
 
-def openfile(file: str, mask: str=None, resized: bool=False) -> None:
+def openfile(file: str, mask: str=None, resized: bool=False, block: bool=False) -> None:
     mask = str(mask) if mask else None
     argv: 'list[str]' = ['-i', file]
     if(mask):
         argv.extend(['-m', mask])
     if(resized):
         argv.append('-r')
-    import micview.run as run
-    run.main(argv)
+
+    import threading
+    from micview.run import main
+
+    if block:
+        main(argv)
+    else:
+        daemon = threading.Thread(target=main, args=(argv,))
+        daemon.setDaemon(True)
+        daemon.start()
+        
