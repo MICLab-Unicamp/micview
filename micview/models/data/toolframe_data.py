9e5f396794b00b92b2ffdc50f4416c274589be38
Caio_Coldebella <caio.r.coldebella@outlook.com>
2023-12-06 20:50:56 -0300

    Filedialogs now opens in the last visited directory

--- micview/models/data/toolframe_data.py
+++ micview/models/data/toolframe_data.py
@@ -4,6 +4,7 @@ class ToolframeDataClass:
         def __init__(self) -> None:
                 super().__init__()
                 self.__channel_intensity: List[Any] = ""
+                self.__dirpath: str = ""
 
         @property
         def channel_intensity(self) -> List[Any]:
@@ -13,3 +14,12 @@ class ToolframeDataClass:
         def channel_intensity(self, value: List[str]) -> None:
                 assert type(value) is str
                 self.__channel_intensity = value
+
+        @property
+        def dirpath(self) -> str:
+                return self.__dirpath
+
+        @dirpath.setter
+        def dirpath(self, value: str) -> None:
+                assert type(value) is str
+                self.__dirpath = value
