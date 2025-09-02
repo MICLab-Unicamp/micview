708edcf39c87d8bde8cdcf039fca699e2de531b7
dscarmo <carmodiedre@outlook.com>
2025-09-02 17:00:21 -0300

    Seems to be working with newer Python version, WIP

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.13',
     install_requires=['setuptools', 'numpy>=1.22', 'pillow>=9.0.0',
     'SimpleITK>=2.3.0', 'screeninfo>=0.8.1', 'pydicom', 'dicom2nifti', 'matplotlib'],
     entry_points={
