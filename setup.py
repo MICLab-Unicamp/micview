83ce8b15fcacd00ca1a4b472586bce227289313f
Letícia Lopes <l184423@dac.unicamp.br>
2026-09-06 10:39:22 -0300

    fix: update Python version requirements to allow only >=3.10

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10, <=3.13',
+    python_requires='>=3.10',
     install_requires=[
         'setuptools',
         'numpy>=1.22',
